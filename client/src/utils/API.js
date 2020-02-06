3383f3edf320144932bf5bb3fac0d225fa3e8af5
joswald123 <48632836+joswald123@users.noreply.github.com>
2020-02-05 17:06:27 -0800

    setted up proxy - package-json file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,21 +4,21 @@ export default {
 
     // Get all Saved Events
     getAllEvents: function() {
-        return axios.get("http://localhost:3001/api/events");
+        return axios.get("/api/events");
     },
     // Get all saved Matches
     getAllMatches:function () {
-        return axios.get("http://localhost:3001/api/match")
+        return axios.get("/api/match")
     },
     
     // Add new Events
     addEvent: function(addEvent) {
-        return axios.post("http://localhost:3001/api/events", addEvent)
+        return axios.post("/api/events", addEvent)
         
     },
     // Add new Matches
     addMatch: function(addMatch) {
-        return axios.post("http://localhost:3001/api/match", addMatch)
+        return axios.post("/api/match", addMatch)
     },
 
 
@@ -26,43 +26,43 @@ export default {
     
     // Get Events by Id
     getEvent: function(id) {
-        return axios.get("http://localhost:3001/api/events" + id);
+        return axios.get("/api/events" + id);
     },
     // Get Matches by id
     getMatch: function(id) {
-        return axios.get("http://localhost:3001/api/match" + id);
+        return axios.get("/api/match" + id);
     },
 
 
 
     // Save an Event  to the database
     saveEvent: function(saveEvent) {
-        return axios.post("http://localhost:3001/api/events", saveEvent);
+        return axios.post("/api/events", saveEvent);
     },
     // Save a Match to the database
     saveMatch: function(saveMatch) {
-        return axios.post("http://localhost:3001/api/match", saveMatch);
+        return axios.post("/api/match", saveMatch);
     },
 
 
 
     // Delete an Event with the given id
     deleteEvent: function(id) {
-        return axios.delete("http://localhost:3001/api/events/" + id);
+        return axios.delete("/api/events/" + id);
     },
     // Delete a Match with given id
     deleteMatch: function(id) {
-        return axios.delete("http://localhost:3001/api/match/" + id);
+        return axios.delete("/api/match/" + id);
     },
 
 
     
     //Update an event 
     updateEvent: function(id) {
-        return axios.put("http://localhost:3001/api/events/" + id);
+        return axios.put("/api/events/" + id);
     },
     //Update a Match
     updateMatch: function(id) {
-        return axios.put("http://localhost:3001/api/match/" + id);
+        return axios.put("/api/match/" + id);
     }
 }
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require("express");
-const cors = require('cors');
 const mongoose = require("mongoose");
-
 require('dotenv').config();
+
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Configure body parsing for AJAX requests
-
-// app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
